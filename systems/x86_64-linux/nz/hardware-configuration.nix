425b0cdced647f667823191eee38ac4673bcf851
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-12-17 10:23:45 +0300

    chore: update flakes and adapt to changes

--- systems/x86_64-linux/nz/hardware-configuration.nix
+++ systems/x86_64-linux/nz/hardware-configuration.nix
@@ -28,29 +28,29 @@
     fsType = "vfat";
   };
 
-  fileSystems."/mnt/tank/torrents" = {
-    device = "192.168.89.200:/mnt/tank/torrents";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.requires=network.target"
-      "x-systemd.mount-timeout=10"
-      "x-systemd.idle-timeout=1min"
-    ];
-    fsType = "nfs";
-  };
+  # fileSystems."/mnt/tank/torrents" = {
+  #   device = "192.168.89.200:/mnt/tank/torrents";
+  #   options = [
+  #     "noauto"
+  #     "x-systemd.automount"
+  #     "x-systemd.requires=network.target"
+  #     "x-systemd.mount-timeout=10"
+  #     "x-systemd.idle-timeout=1min"
+  #   ];
+  #   fsType = "nfs";
+  # };
 
-  fileSystems."/mnt/obsidian" = {
-    device = "192.168.89.200:/mnt/tank/Apps/obsidian";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.requires=network.target"
-      "x-systemd.mount-timeout=10"
-      "x-systemd.idle-timeout=1min"
-    ];
-    fsType = "nfs";
-  };
+  # fileSystems."/mnt/obsidian" = {
+  #   device = "192.168.89.200:/mnt/tank/Apps/obsidian";
+  #   options = [
+  #     "noauto"
+  #     "x-systemd.automount"
+  #     "x-systemd.requires=network.target"
+  #     "x-systemd.mount-timeout=10"
+  #     "x-systemd.idle-timeout=1min"
+  #   ];
+  #   fsType = "nfs";
+  # };
 
   swapDevices = [
     {device = "/dev/disk/by-uuid/73002676-c9b2-46e8-a987-5bf002c91cbc";}
@@ -60,24 +60,26 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  networking.hosts = {
-    # "192.168.89.200" = ["truenas.sbulav.ru"];
-    "100.87.128.100" = ["vault-c7.pyn.ru"];
-    "100.91.128.100" = ["vault-c11.pyn.ru"];
-    "100.92.128.100" = ["vault-c12.pyn.ru"];
-    "192.168.89.207" = ["zanoza" "jellyfin.sbulav.ru" "authelia.sbulav.ru" "home.sbulav.ru"];
+  networking = {
+    useDHCP = lib.mkDefault true;
+    hosts = {
+      "100.87.128.100" = ["vault-c7.pyn.ru"];
+      "100.91.128.100" = ["vault-c11.pyn.ru"];
+      "100.92.128.100" = ["vault-c12.pyn.ru"];
+      "192.168.89.207" = ["zanoza" "jellyfin.sbulav.ru" "authelia.sbulav.ru" "home.sbulav.ru"];
+    };
+    # interfaces.wlp3s0.ipv4.routes = [
+    #   {
+    #     address = "192.168.89.0";
+    #     prefixLength = 24;
+    #     via = "192.168.88.1";
+    #   }
+    # ];
+    search = ["sbulav.ru"];
+    # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
+    # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
   };
-  networking.interfaces.wlp3s0.ipv4.routes = [
-    {
-      address = "192.168.89.0";
-      prefixLength = 24;
-      via = "192.168.88.1";
-    }
-  ];
-  # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
