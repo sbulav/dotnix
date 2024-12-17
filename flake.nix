87fd3bd36b43633381a959ea434587a9f2b9d871
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2024-12-17 10:50:33 +0300

    fix(zoom): stick to 24.05 as zoom screensharing is still broken

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
     snowfall-lib = {
       url = "github:snowfallorg/lib?ref=v3.0.3";
