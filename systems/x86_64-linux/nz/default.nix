06a76742435dd7078a581ee856604511b4ea3706
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-01-14 13:32:32 +0300

    feat(nz): limit journal size

--- systems/x86_64-linux/nz/default.nix
+++ systems/x86_64-linux/nz/default.nix
@@ -34,6 +34,14 @@ in {
     podman.enable = true;
   };
 
+  # limit systemd journal size
+  # https://wiki.archlinux.org/title/Systemd/Journal#Persistent_journals
+  services.journald.extraConfig = ''
+    SystemMaxUse=100M
+    RuntimeMaxUse=50M
+    SystemMaxFileSize=50M
+  '';
+
   # ======================== DO NOT CHANGE THIS ========================
   system.stateVersion = "23.11";
   # ======================== DO NOT CHANGE THIS ========================
