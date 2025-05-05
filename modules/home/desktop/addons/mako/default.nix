46cab90b42d4e4b1d2077de6b2fd4d185f68406e
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-05-05 09:52:30 +0300

    chore: update flake and adapt to new settings

--- modules/home/desktop/addons/mako/default.nix
+++ modules/home/desktop/addons/mako/default.nix
@@ -19,33 +19,35 @@ in {
     ];
     services.mako = {
       enable = true;
-      defaultTimeout = 5000;
-      font = "FiraCode Nerd Font 10";
-      layer = "overlay";
-      anchor = "top-right";
-      maxIconSize = 32;
-      icons = true;
-      iconPath = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
-      borderRadius = 5;
-      margin = "5";
-      extraConfig = ''
-        group-by=app-name
-        background-color=#FFFFFFee
-        text-color=#000000cc
-        border-color=#00000033
-        border-size=1
-        progress-color=source #07b5efee
-
-        [urgency=low]
-        border-color=#394b70
-
-        [urgency=normal]
-        border-color=#00000033
-
-        [urgency=high]
-        border-color=#ff757f
-        default-timeout=0
-      '';
+      settings = {
+        background-color = "#FFFFFFee";
+        border-color = "#00000033";
+        border-size = "1";
+        defaultTimeout = 5000;
+        group-by = "app-name";
+        progress-color = "source"; #07b5efee
+        text-color = "#000000cc";
+        anchor = "top-right";
+        borderRadius = 5;
+        font = "FiraCode Nerd Font 10";
+        iconPath = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
+        icons = true;
+        layer = "overlay";
+        margin = "5";
+        maxIconSize = 32;
+      };
+      criteria = {
+        "urgency=high" = {
+          border-color = "#394b70";
+          default-timeout = 0;
+        };
+        "urgency=normal" = {
+          border-color = "#00000033";
+        };
+        "urgency=low" = {
+          border-color = "#ff757f";
+        };
+      };
     };
   };
 }
