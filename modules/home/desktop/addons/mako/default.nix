86862b43d22bcdfa3ad31af9785dee7678252722
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-05-20 16:42:05 +0300

    fix(mako): update flake and adapt to new mako settings

--- modules/home/desktop/addons/mako/default.nix
+++ modules/home/desktop/addons/mako/default.nix
@@ -20,23 +20,21 @@ in {
     services.mako = {
       enable = true;
       settings = {
+        anchor = "top-right";
         background-color = "#FFFFFFee";
         border-color = "#00000033";
+        border-radius = 5;
         border-size = "1";
-        defaultTimeout = 5000;
-        group-by = "app-name";
-        progress-color = "source"; #07b5efee
-        text-color = "#000000cc";
-        anchor = "top-right";
-        borderRadius = 5;
+        default-timeout = 5000;
         font = "FiraCode Nerd Font 10";
-        iconPath = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
+        group-by = "app-name";
+        icon-path = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
         icons = true;
         layer = "overlay";
         margin = "5";
-        maxIconSize = 32;
-      };
-      criteria = {
+        progress-color = "source #07b5efee"; #07b5efee
+        text-color = "#000000cc";
+        max-icon-size = 32;
         "urgency=high" = {
           border-color = "#394b70";
           default-timeout = 0;
