21b4a48642c4c43510c717877f789a2d1c6759e7
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-11-28 10:31:01 +0300

    fix: disable all cachix substituters for now

--- modules/nixos/system/nix/default.nix
+++ modules/nixos/system/nix/default.nix
@@ -57,21 +57,21 @@ in
           substituters = [
             "https://cache.nixos.org"
             "https://install.determinate.systems"
-            "https://nix-community.cachix.org"
-            "https://dotnix.cachix.org"
-            "https://nixpkgs-unfree.cachix.org"
-            "https://numtide.cachix.org"
-            "https://wezterm.cachix.org"
+            # "https://nix-community.cachix.org"
+            # "https://dotnix.cachix.org"
+            # "https://nixpkgs-unfree.cachix.org"
+            # "https://numtide.cachix.org"
+            # "https://wezterm.cachix.org"
           ];
 
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-            "dotnix.cachix.org-1:/T5Rhb8DkIIAU5wwL2YnMqMsNUkIcOxCIaHUKSaLAVs="
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-            "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-            "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+            # "dotnix.cachix.org-1:/T5Rhb8DkIIAU5wwL2YnMqMsNUkIcOxCIaHUKSaLAVs="
+            # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            # "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+            # "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+            # "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
           ];
         };
 
