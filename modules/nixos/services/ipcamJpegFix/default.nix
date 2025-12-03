15201bca337e5cad91bcec53ee973ffc062cd304
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2025-12-03 16:37:18 +0300

    fix(ipcamJpegFix): perms

--- modules/nixos/services/ipcamJpegFix/default.nix
+++ modules/nixos/services/ipcamJpegFix/default.nix
@@ -77,8 +77,8 @@ in
 
               if cat "$tmpfile" > "$jpgfile"; then
                 touch -r "$tmpfile" "$jpgfile" || true
-                chmod --reference="$tmpfile" "$jpgfile" 2>/dev/null || true
-                chown --reference="$tmpfile" "$jpgfile" 2>/dev/null || true
+                # chmod --reference="$tmpfile" "$jpgfile" 2>/dev/null || true
+                chown nobody:nogroup "$jpgfile" 2>/dev/null || true
                 converted=$((converted + 1))
                 # printf 'Converted: %s\n' "$jpgfile"
               else
