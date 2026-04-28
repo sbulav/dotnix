a609c9c8f4b0866acc4e14efa00666bd4840b035
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2026-04-28 11:29:51 +0300

    feat(wezterm): use neon wezterm theme

--- modules/home/desktop/addons/wezterm/default.nix
+++ modules/home/desktop/addons/wezterm/default.nix
@@ -9,6 +9,7 @@ with lib;
 with lib.custom;
 let
   cfg = config.custom.desktop.addons.wezterm;
+  c = config.custom.theme.colors;
 in
 {
   options.custom.desktop.addons.wezterm = {
@@ -21,7 +22,26 @@ in
       # package = inputs.wezterm.packages.${pkgs.stdenv.hostPlatform.system}.default;
       extraConfig =
         # Generate wezterm.lua; order of files are important
-        (builtins.readFile ./wezterm.lua)
+        ''
+          local custom_theme = {
+            base = "#${c.base}",
+            panel = "#${c.panel}",
+            elevated = "#${c.elevated}",
+            text = "#${c.text}",
+            subtext = "#${c.subtext}",
+            separator = "#${c.separator}",
+            cyan = "#${c.cyan}",
+            pink = "#${c.pink}",
+            violet = "#${c.violet}",
+            mint = "#${c.mint}",
+            amber = "#${c.amber}",
+            blue = "#${c.blue}",
+            overlay0 = "#${c.overlay0}",
+            overlay1 = "#${c.overlay1}",
+            overlay2 = "#${c.overlay2}",
+          }
+        ''
+        + (builtins.readFile ./wezterm.lua)
         + (builtins.readFile ./mappings.lua)
         + (builtins.readFile ./colors.lua)
         + (builtins.readFile ./tabs.lua)
