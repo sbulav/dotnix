e85cb4d5888d2c4e9a02a6cc50d818a374305fa2
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2026-07-14 12:16:10 +0300

    chore(herdr-remote): bump to TUI chrome fix

--- packages/herdr-relay/default.nix
+++ packages/herdr-relay/default.nix
@@ -8,7 +8,7 @@
 }:
 python3Packages.buildPythonApplication {
   pname = "herdr-relay";
-  version = "0.5.0-unstable-2026-07-13";
+  version = "0.5.0-unstable-2026-07-14";
 
   src = inputs.herdr-remote;
   format = "other";
