df02a19b4ee3af5a2815b8657c370f5278d3e035
Sergei Bulavintsev <bulavintsev.sergey@gmail.com>
2026-07-14 20:48:38 +0300

    chore(herdr-remote): label power control revision

--- packages/herdr-relay/default.nix
+++ packages/herdr-relay/default.nix
@@ -8,7 +8,7 @@
 }:
 python3Packages.buildPythonApplication {
   pname = "herdr-relay";
-  version = "0.5.0-unstable-2026-07-14.db0d29f";
+  version = "0.5.0-unstable-2026-07-14.972a7b4";
 
   src = inputs.herdr-remote;
   format = "other";
